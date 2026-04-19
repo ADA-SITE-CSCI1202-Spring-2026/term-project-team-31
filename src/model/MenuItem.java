706b439eac41ec1fdd5b4006f55853361278a5c0
Fuad Karimli <cyberpulse@Fuads-MacBook-Air.local>
2026-04-19 21:11:30 +0400

    Added the MenuItem File

--- src/model/MenuItem.java
+++ src/model/MenuItem.java
@@ -1,5 +1,34 @@
 package model;
 
-public class MenuItem {
+import java.util.Map;
+
+
+public abstract class MenuItem {
+
+    private String name;
+
+    public MenuItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     
+    public abstract Map<Ingredient, Integer> getRequiredIngredients();
+
+   
+    public abstract String getTypeTag();
+
+    
+    public abstract String getRequiredProcessorType();
+
+   
+    public abstract double getRevenue();
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
