b4e893adee70c7c0b65cfccc3305cb2c5e966d18
KamalHasanov15271 <147622879+Kamal0085@users.noreply.github.com>
2026-04-23 09:30:47 +0400

    updated Burger.java

--- src/model/Burger.java
+++ src/model/Burger.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Burger {
-    
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class Burger extends MenuItem {
+
+    public Burger() {
+        super("Cheeseburger");
+    }
+
+    @Override
+    public Map<Ingredient, Integer> getRequiredIngredients() {
+        Map<Ingredient, Integer> req = new LinkedHashMap<>();
+        req.put(Ingredient.BUNS,    1);
+        req.put(Ingredient.PATTIES, 1);
+        req.put(Ingredient.CHEESE,  1);
+        req.put(Ingredient.LETTUCE, 1);
+        return req;
+    }
+
+    @Override
+    public String getTypeTag() {
+        return "BURGER";
+    }
+
+    @Override
+    public String getRequiredProcessorType() {
+        return "GRILL";
+    }
+
+    @Override
+    public double getRevenue() {
+        return 8.50;
+    }
 }
