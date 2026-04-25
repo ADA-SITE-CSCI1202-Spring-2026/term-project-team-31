67cdf8dd491a8b6cc5f9b495761dbd96e4bf6bd8
Saonedd <95407002+sabiristaken@users.noreply.github.com>
2026-04-25 23:32:29 +0400

    Update Beverage.java

--- src/model/Beverage.java
+++ src/model/Beverage.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Beverage {
-    
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class Beverage extends MenuItem {
+
+    public Beverage() {
+        super("Fountain Drink");
+    }
+
+    @Override
+    public Map<Ingredient, Integer> getRequiredIngredients() {
+        Map<Ingredient, Integer> req = new LinkedHashMap<>();
+        req.put(Ingredient.COLA_SYRUP, 1);
+        req.put(Ingredient.CUPS,       1);
+        return req;
+    }
+
+    @Override
+    public String getTypeTag() {
+        return "BEVERAGE";
+    }
+
+    @Override
+    public String getRequiredProcessorType() {
+        return "DISPENSER";
+    }
+
+    @Override
+    public double getRevenue() {
+        return 2.50;
+    }
 }
