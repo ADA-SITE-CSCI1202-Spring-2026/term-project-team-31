04811912358d35df26408a7755985dfdcc47106f
Fuad Karimli <cyberpulse@Fuads-MacBook-Air.local>
2026-04-26 20:43:18 +0400

    Added Fried Chicken File

--- src/model/FriedChicken.java
+++ src/model/FriedChicken.java
@@ -1,5 +1,37 @@
+
+
 package model;
 
-public class FriedChicken {
-    
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class FriedChicken extends MenuItem {
+
+    public FriedChicken() {
+        super("Fried Chicken");
+    }
+
+    @Override
+    public Map<Ingredient, Integer> getRequiredIngredients() {
+        Map<Ingredient, Integer> req = new LinkedHashMap<>();
+        req.put(Ingredient.CHICKEN, 2);
+        req.put(Ingredient.OIL,     1);
+        return req;
+    }
+
+    @Override
+    public String getTypeTag() {
+        return "FRIED_CHICKEN";
+    }
+
+    @Override
+    public String getRequiredProcessorType() {
+        return "FRYER";
+    }
+
+    @Override
+    public double getRevenue() {
+        return 7.00;
+    }
 }
