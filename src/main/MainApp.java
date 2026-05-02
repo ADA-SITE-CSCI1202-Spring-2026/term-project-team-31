651a1f59dfe698e2163335600dccbb9473971d11
Saonedd <95407002+sabiristaken@users.noreply.github.com>
2026-05-03 00:26:53 +0400

    Update MainApp.java

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,5 +1,29 @@
 package main;
 
-public class MainApp {
-    
+import gui.Dashboard;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Dashboard dashboard = new Dashboard();
+
+        Scene scene = new Scene(dashboard, 900, 620);
+        primaryStage.setTitle("Program intialized");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(550);
+
+        primaryStage.setOnCloseRequest(e -> dashboard.stopClock());
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
